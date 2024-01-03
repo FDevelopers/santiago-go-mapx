12f050c13027fec41347e43dc84b3f6582ccc2b7
Fernando Disla <fdisla1608@gmail.com>
2024-01-02 21:15:48 -0500

    fix the routes and location

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  Polyline,
+} from "@react-google-maps/api";
+import axios from "axios";
 
-function App() {
+const libraries = ["places"];
+const mapContainerStyle = {
+  width: "100vw",
+  height: "100vh",
+};
+const center = {
+  lat: 19.441261067653702, // default latitude
+  lng: -70.68447494396473, // default longitude
+};
+
+const App = () => {
+  const [rutas, setRutas] = useState();
+  const [lat, setLat] = useState(19.441907556432835);
+  const [lgn, setLgn] = useState(-70.6815231746255);
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: "AIzaSyARFCccvBa5znIAbFaMotUz6MfPh_doCrg",
+    libraries,
+  });
+
+  useEffect(() => {
+    const getLocation = () => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            const { latitude, longitude } = position.coords;
+            setLat(latitude);
+            setLgn(longitude);
+          },
+          (error) => {
+            console.error("Error getting location:", error.message);
+          }
+        );
+      } else {
+        console.error("Geolocation is not supported by this browser.");
+      }
+
+      getLocation();
+    };
+
+    axios
+      .get(`http://127.0.0.1:3001/api/maps/rutax`)
+      .then((res) => {
+        console.log(res.data[0].colorRutas);
+        setRutas(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  if (loadError) {
+    return <div>Error loading maps</div>;
+  }
+
+  if (!isLoaded) {
+    return <div>Loading maps</div>;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={18}
+        center={center}
+      >
+        <Marker position={{ lat: lat, lng: lgn }} />
+        <Polyline
+          path={rutas && rutas[0].coordinates}
+          strokeColor="#0FC72E"
+          strokeOpacity={0.8}
+          strokeWeight={2}
+        />
+
+        <Polyline
+          path={rutas && rutas[1].coordinates}
+          strokeColor={rutas && rutas[1].colorRutas}
+          strokeOpacity={0.8}
+          strokeWeight={2}
+        />
+      </GoogleMap>
     </div>
   );
-}
+};
 
 export default App;
